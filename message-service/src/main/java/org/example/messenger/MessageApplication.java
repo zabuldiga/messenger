74f05cc12ee1d28e0685ed6a049eefa8c294d9da
alexander <al.zhuk98@gmail.com>
2026-02-06 14:55:22 +0300

    create message-service

---
+++ message-service/src/main/java/org/example/messenger/MessageApplication.java
@@ -0,0 +1,12 @@
+package org.example.messenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MessageApplication.class);
+    }
+}
+
